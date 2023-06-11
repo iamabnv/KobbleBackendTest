22ffd37e519198ebbd9f03bc1ba4f642d94f407c
iamabnv <abhnv@seas.upenn.edu>
2023-06-10 20:17:12 -0400

    initial commit

---
+++ KobbleBackendTest/KobbleBackendTest/KobbleBackendTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KobbleBackendTestApp.swift
+//  KobbleBackendTest
+//
+//  Created by Abhinav Srivastava on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct KobbleBackendTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
